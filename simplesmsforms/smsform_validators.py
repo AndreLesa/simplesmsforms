515fef0373ea1d72770553de8383cf8424ea33c1
Andre Lesa <andre@andrelesa.com>
2015-01-19 06:20:02 +0200

    lower case incoming values in validation.

--- simplesmsforms/smsform_validators.py
+++ simplesmsforms/smsform_validators.py
@@ -6,12 +6,20 @@ from smsform_exceptions import (ChoiceException, InvalidDateException,
 """Validators should have a simple signature accepting any number of arguments
 to make processing them easier"""
 
+class LowerCaseException(Exception):
+    pass
+
+def lowercase_list_util(value_list):
+    try:
+        return [value.lower() for value in value_list]
+    except AttributeError:
+        raise LowerCaseException("%s can not be lower cased"%value_list)
 
 def multiple_choice_validator(*args, **kwargs):
     """Takes a single value or a list of values and validates that they are all
     part of a set of choices"""
-    value = set(kwargs["value"])
-    choices = set(kwargs["choices"])
+    value = set(lowercase_list_util(kwargs["value"]))
+    choices = set(lowercase_list_util(kwargs["choices"]))
     if not value.issubset(choices):
         raise ChoiceException("Invalid option '{value}', please select one of: {choices_string}".format(
                 value=", ".join(value),
